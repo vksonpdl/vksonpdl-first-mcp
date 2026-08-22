1bd2de28f6b1db9c5a9eae4b9d7c47c79218ca9d
vksonpdl <vksonpdl@gmail.com>
2026-08-22 12:57:46 +0530

    vksonpdl-first-mcp : enabled auth

--- src/main/java/vksonpdl/firstmcp/OAuthMetadataController.java
+++ src/main/java/vksonpdl/firstmcp/OAuthMetadataController.java
@@ -1,10 +1,11 @@
-/*
 package vksonpdl.firstmcp;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
 
@@ -14,15 +15,26 @@ public class OAuthMetadataController {
     @Value("${spring.security.jwt.issuer-uri}")
     private String issuerUri;
 
-    @Value("${spring.security.jwt.resource}")
-    private String resource;
+    @Value("${spring.security.jwt.authorize-uri}")
+    private String authorizeUri;
 
-    @GetMapping("/.well-known/oauth-protected-resource")
-    public Map<String, Object> getProtectedResourceMetadata() {
-        return Map.of(
-                "resource", resource,
-                "authorization_servers", List.of(issuerUri)
-        );
+    @Value("${spring.security.jwt.token-uri}")
+    private String tokenUri;
+
+    @Value("${spring.security.jwt.jwks-uri}")
+    private String jwksUri;
+
+    @GetMapping("/.well-known/oauth-authorization-server")
+    public ResponseEntity<Map<String, Object>> getOAuthMetadata() {
+        Map<String, Object> metadata = new HashMap<>();
+
+        // Point Claude directly to your Auth0 Tenant
+        metadata.put("issuer", issuerUri);
+        metadata.put("authorization_endpoint",authorizeUri);
+        metadata.put("token_endpoint", tokenUri);
+        metadata.put("jwks_uri", jwksUri);
+        metadata.put("response_types_supported", List.of("code"));
+
+        return ResponseEntity.ok(metadata);
     }
 }
-*/
