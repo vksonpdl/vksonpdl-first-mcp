367cba94ea1998c41e5bf2916c60e35e408492fa
vksonpdl <vksonpdl@gmail.com>
2026-09-18 14:42:05 +0530

    vksonpdl-first-mcp : enabled auth

--- src/main/java/vksonpdl/firstmcp/oauth/SecurityConfig.java
+++ src/main/java/vksonpdl/firstmcp/oauth/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.*;
@@ -27,14 +28,18 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http,JwtDecoder jwtDecoder) throws Exception  {
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/health").permitAll()
                         .anyRequest().authenticated()
                 )
+                // Configure standard Spring Security to use the custom decoder
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.decoder(jwtDecoder()))
+                )
                 .with(
                         McpServerOAuth2Configurer.mcpServerOAuth2(),
                         (mcpAuthorization) -> {
-                            mcpAuthorization.jwtDecoder(jwtDecoder);
                             mcpAuthorization.authorizationServer(issuer);
                             mcpAuthorization.validateAudienceClaim(true);
                         }
@@ -49,7 +54,7 @@ public class SecurityConfig {
 
         OAuth2TokenValidator<Jwt> audienceValidator = new JwtClaimValidator<List<String>>(
                 JwtClaimNames.AUD,
-                aud -> aud != null && aud.contains(issuer)
+                aud -> aud != null && aud.contains(audiance)
         );
         OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
         OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
