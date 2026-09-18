695f5106c2f0213b6c9f9a87b3ae5bb5acb9f78e
vksonpdl <vksonpdl@gmail.com>
2026-09-18 14:43:23 +0530

    vksonpdl-first-mcp : enabled auth

--- src/main/java/vksonpdl/firstmcp/oauth/SecurityConfig.java
+++ src/main/java/vksonpdl/firstmcp/oauth/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
 
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http,JwtDecoder jwtDecoder) throws Exception  {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception  {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
